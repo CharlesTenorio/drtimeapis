f60c4fa71f15d5140eafbf3b1fc1559d62b87123
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2020-01-19 23:05:17 -0300

    seeting banco

--- drtimeapis/settings.py
+++ drtimeapis/settings.py
@@ -96,12 +96,9 @@ WSGI_APPLICATION = 'drtimeapis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
 # Password validation
